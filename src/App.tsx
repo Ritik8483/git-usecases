43d7d28763110bee6b67706aff1bf1c50463dd19
Ritik Chauhan <ritik.chauhan@quokkalabs.com>
2024-03-05 17:21:31 +0530

    (feat): first commit by own ritik

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ width: "100%" }}>
+      Git commands
+    </div>
+  );
+}
+
+export default App;
+
+
+// 1. To add local repo to remote repo
+//npm init
