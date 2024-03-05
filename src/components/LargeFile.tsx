9ca30ec4a10662ddf8722612896650126656a408
Ritik Chauhan <ritik.chauhan@quokkalabs.com>
2024-03-05 17:44:00 +0530

    (feat): add video tag without including videos in the code and commit

--- src/components/LargeFile.tsx
+++ src/components/LargeFile.tsx
@@ -8,6 +8,12 @@ const LargeFile = () => {
         alignItems: "center",
       }}
     >
+      <video
+        src="https://d2c0tajxhzjrh8.cloudfront.net/static/4cf20923-89db-412a-86af-ab454389cae9.mp4"
+        height="300"
+        width="300"
+        controls
+      />
       <h1>LargeFile is not uploaded yet. </h1>
       <h6>
         (Its code of first commit where large file is not included in the commit
