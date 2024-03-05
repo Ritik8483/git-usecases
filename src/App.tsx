e07b0cbf9d47514e1128fcf1205b879360ad010f
Ritik Chauhan <ritik.chauhan@quokkalabs.com>
2024-03-05 17:27:07 +0530

    (feat): write all commands related to setting up an repo in app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -10,4 +10,8 @@ export default App;
 
 
 // 1. To add local repo to remote repo
-//npm init
+//git init (Initialized empty Git repository locally)
+//git commit -m "first commit" (It will save that code locally)
+//git remote add origin https://github.com/Ritik8483/git-usecases.git (It will add origin to the remote repo so that local code can go on server)
+//git remote -v (To check origin of added repo)
+//git push -u origin master (It will push the code to the master branch) 
